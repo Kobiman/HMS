053123c08f94907ceb4324160dba95c954dca41c
Kobina Lawson <kobinalawson@hotmail.com>
2021-06-20 21:31:35 +0000

    Menu

---
+++ HMS/Shared/Models/Attendance.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Shared.Models
+{
+    public class Attendance
+    {
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ HMS/Shared/Models/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMS.Shared.Models
+{
+    public class Staff
+    {
+        public string Surname { get; set; }
+        public string Othernames { get; set; }
+        public string DateofBirth { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
