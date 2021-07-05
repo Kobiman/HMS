eea24b0271861546dda622d3b0f1297b6dbdaddb
mryawson <webmategh@gmail.com>
2021-07-05 07:20:18 -0700

    All modues ui done

---
+++ ERP/Shared/Models/EmployeeBenefit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Shared.Models
+{
+   public class EmployeeBenefit
+    {
+        public int Id { get; set; }
+        public String Benefits { get; set; }
+        public String Category { get; set; }
+        public double Rate { get; set; }
+        public String  Code { get; set; }
+    }
+}
---
+++ ERP/Shared/Models/EmployeeDeduction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Shared.Models
+{
+   public class EmployeeDeduction
+    {
+        public int Id { get; set; }
+        public String Deductions { get; set; }
+        public double Rate { get; set; }
+        public String  Code { get; set; }
+    }
+}
