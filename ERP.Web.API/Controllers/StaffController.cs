fc106cf4117df890a0662f8a6f41ab639d38bb91
Kobina Lawson <kobinalawson@hotmail.com>
2021-07-25 12:15:16 +0000

    ERP.HRM.Web.API

---
+++ ERP.Web.API/Controllers/StaffController.cs
@@ -0,0 +1,58 @@
+﻿using ERP.HRM.Services.Contracts;
+using ERP.Models.HMS;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERP.HRM.Web.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly IStaffService _staffService;
+        public StaffController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+
+        [HttpPost]
+        [Route("AddStaff")]
+        public async Task<IActionResult> AddStaff([FromBody] Staff staff)
+        {
+            var result = await _staffService.AddStaff(staff);
+            if (result.IsSucessful) return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpPost]
+        [Route("UpdateStaff")]
+        public async Task<IActionResult> UpdateStaff([FromBody] Staff staff)
+        {
+            var result = await _staffService.UpdateStaff(staff);
+            if (result.IsSucessful) return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpGet]
+        [Route("GetStaffs")]
+        public async Task<IActionResult> GetStaffs()
+        {
+            var result = await _staffService.GetStaffs();
+            if (result.IsSucessful) return Ok(result);
+            return BadRequest(result);
+        }
+
+        [HttpGet]
+        [Route("GetStaff/{stffId}")]
+        public async Task<IActionResult> GetStaff(string stffId)
+        {
+            var result = await _staffService.GetStaff(stffId);
+            if (result.IsSucessful) return Ok(result);
+            return BadRequest(result);
+        }
+    }
+}
---
+++ ERP.Web.API/Startup.cs
@@ -0,0 +1,69 @@
+using ERP.HRM.Data;
+using ERP.HRM.Data.Contracts;
+using ERP.HRM.Services;
+using ERP.HRM.Services.Contracts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERP.HRM.Web.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<HMSDataContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddTransient<IStaffService, StaffService>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "ERP.Web.API", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ERP.Web.API v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
